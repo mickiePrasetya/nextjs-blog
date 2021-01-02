42dfa3e52171c0f1a75032c6383c766eda2236d3
DESKTOP-963FGG6\Owlstudio <owlstudio.dsign@gmail.com>
2021-01-02 10:04:55 +0700

    bugfixing import file Layout

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import Layout from '../../components/Layout'
+import Layout from '../../components/layout'
 import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
